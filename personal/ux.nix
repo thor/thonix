e23245b38afc6c7fc3db079df0e624ccb60dcb4f
Thor K. Høgås <thor@roht.no>
2025-01-29 23:21:44 +0100

    fix: add fantasque sans mono

--- personal/ux.nix
+++ personal/ux.nix
@@ -1,5 +1,12 @@
 { config, pkgs, ... }:
+let
+  fonts = with pkgs; [
+    fantasque-sans-mono
+  ];
+in
 {
+  # Setup my fants
+  fonts.packages = fonts;
   environment.systemPackages = with pkgs; [
     # app starter which is much more useful than spotlight
     raycast
