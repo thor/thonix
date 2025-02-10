9cc41e1691d79bceafe8e2dc075e07dc94143885
Thor K. Høgås <thor@roht.no>
2025-02-10 17:23:32 +0100

    fix(work): add various work tools

--- work/default.nix
+++ work/default.nix
@@ -4,14 +4,29 @@
 }:
 
 {
+  # I need unfree packages to work with work
+  nixpkgs.config.allowUnfree = true;
+
   # nix packages
   environment.systemPackages = with pkgs; [
     # source control
     git
+    github-cli
     # communication tool
     slack
     # yummy python
     pipx
+    # general good infra stuff
+    terraform
+    terraform-lsp
+    terraform-docs
+    # grafana stack tools
+    grafana-loki
+    grafana-alloy
+    grizzly
+    # mandatory google
+    google-cloud-sdk
+    google-cloud-sql-proxy
   ];
 
   # brew formulae and casks
