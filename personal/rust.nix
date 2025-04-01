c81d3ed55fc53e2b477e9b02c0959838ad3b49af
Thor K. Høgås <thor@roht.no>
2025-04-01 09:04:22 +0200

    fix: add gcc for rust

--- personal/rust.nix
+++ personal/rust.nix
@@ -2,6 +2,7 @@
 {
   # Barebones rust setup, limited to the analyzer for the time being in terms of fenix features
   environment.systemPackages = with pkgs; [
+    gcc # added because a rust component needed it
     rustup
     rust-analyzer-nightly
   ];
