1b79ead9f08a78507aee9ca60567e722263b2409
Thor K. Høgås <thor@roht.no>
2025-08-22 18:00:48 +0200

    fix(brew): remove unused bundle tap

--- personal/brew.nix
+++ personal/brew.nix
@@ -28,7 +28,6 @@
     taps = {
       "homebrew/homebrew-core" = inputs.homebrew-core;
       "homebrew/homebrew-cask" = inputs.homebrew-cask;
-      "homebrew/homebrew-bundle" = inputs.homebrew-bundle;
       "Kegworks-App/homebrew-kegworks" = inputs.kegworks;
     };
 
