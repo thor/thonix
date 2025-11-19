d8c7318dc6cced69d3909b3560da5f969daeaf39
Thor K. Høgås <thor@roht.no>
2025-11-19 12:06:28 +0100

    misc: update most flakes and add antigravity

--- personal/llm.nix
+++ personal/llm.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  inherit (lib) mkIf;
+  inherit (pkgs.stdenv) isDarwin;
+in
 {
   environment.systemPackages = with pkgs; [
     ollama # local models
     gemini-cli # open source cli with gemini access and mcp
+  ];
+
+  homebrew.casks = mkIf isDarwin [
+    "antigravity"
   ];
 }
