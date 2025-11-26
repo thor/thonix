971b4121b6fc32e41d5cb324836ca7f65d56acb8
Thor K. Høgås <thor@roht.no>
2025-11-26 16:07:19 +0100

    chore: system => stdenv.hostPlatform.system

--- flake.nix
+++ flake.nix
@@ -57,11 +57,11 @@
     in
     {
       # for `nix fmt`
-      formatter = eachSystem (pkgs: treefmtEval.${pkgs.system}.config.build.wrapper);
+      formatter = eachSystem (pkgs: treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.wrapper);
 
       # for `nix flake check`
       checks = eachSystem (pkgs: {
-        formatting = treefmtEval.${pkgs.system}.config.build.check self;
+        formatting = treefmtEval.${pkgs.stdenv.hostPlatform.system}.config.build.check self;
       });
 
       # for `darwin-rebuild build --flake .#lincoln-golf`
