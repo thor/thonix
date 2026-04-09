5cba99d51f05e11e963ea1b6595eb1dbaa2dec10
Thor K. Høgås <thor@roht.no>
2026-04-09 11:53:32 +0200

    fix(dev): point .test to local dnsmasq and let service handle .internal
    Do not supply .internal: all .internal domains are handled nonetheless.

--- personal/development.nix
+++ personal/development.nix
@@ -1,11 +1,20 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
   # Enable dnsmasq for local development
   services.dnsmasq = {
     enable = true;
     addresses = {
-      ".internal" = "127.0.0.1";
+      "internal" = "127.0.0.1";
     };
     port = 5553;
+  };
+
+  # macOS uses /etc/resolver/<domain> files for per-domain DNS servers.
+  # This is for local testing where I point it to external addresses, too.
+  environment.etc = lib.mkIf pkgs.stdenv.isDarwin {
+    "resolver/test".text = ''
+      nameserver 127.0.0.1
+      port 5554
+    '';
   };
 }
